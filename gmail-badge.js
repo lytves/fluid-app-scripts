ad0a4d34cdf50da71e7a4199545afda62ab8fbc9
Lytves <22356770+lytves@users.noreply.github.com>
2019-09-27 21:40:39 +0200

    Update gmail-badge.js

--- gmail-badge.js
+++ gmail-badge.js
@@ -8,7 +8,9 @@ setInterval(updateDockBadge, 15000);
 function updateDockBadge() {
   window.fluid.dockBadge = '';
 
-  var res = document.title.match(/\s*Inbox\s*\(([0-9,]+)\).*/);
+  // var res = document.title.match(/\s*Inbox\s*\(([0-9,]+)\).*/);
+  // IMPORTANT: you should turn on New mail notifications on on Gmail-Settings-Advanced Settings 
+   var res = document.title.match(/\(([0-9,]+)\)/);
 
   if (res && res.length > 1) {
     var newBadge = res[1];
